250bd8631f6ae2e0e5699848c8344427209c8407
Benjamin Giller <bgiller@berkeley.edu>
2020-07-07 11:19:19 -0700

    trip schema

---
+++ models/Trip.js
@@ -0,0 +1,92 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const TripSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    // flight object
+    flight: {
+        startLocation: {
+            type: String,
+            required: true
+        },
+        endLocation: {
+            type: String,
+            required: true
+        },
+
+        startDate: {
+            type: Date,
+            required: true
+        },
+
+        endDate: {
+            type: Date,
+            required: true
+        },
+
+        price: {
+            type: String,
+            required: true
+        },
+
+        bookingUrl: {
+            type:String,
+            required: true
+        }
+
+    },
+
+    //housing object
+
+    housing: {
+        location: {
+            type: String,
+            required: true
+        },
+
+        price: {
+            type: String,
+            required: true
+        },
+
+        startDate: {
+            type: Date,
+            required: true
+        },
+
+        endDate: {
+            type: Date,
+            required: true
+        }
+
+
+
+    },
+
+    event: {
+        location: {
+            type: String,
+            required: true
+        },
+
+        price: {
+            type: String,
+            required: true
+        },
+
+        startDate: {
+            type: Date,
+            required: true
+        },
+
+        endDate: {
+            type: Date,
+            required: true
+        }
+    }
+
+
+})
