fb80aedaea12b7d43c45f0fc09a007c8cc20182b
pr0grara <azbaghda@gmail.com>
2020-07-07 18:45:20 -0700

    heroku push 5

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import axios from 'axios'
 import Root from './components/root';
 import configureStore from './store/store';
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,13 +1,13 @@
 import { combineReducers } from 'redux';
 
-import entitiesReducer from './entities_reducer'
+//import entitiesReducer from './entities_reducer'
 import sessionReducer from './session_reducer'
-import errorsReducer from './errors_reducer'
+//import errorsReducer from './errors_reducer'
 
 const rootReducer = combineReducers({
-  entities: entitiesReducer,
+  //entities: entitiesReducer,
   session: sessionReducer,
-  errors: errorsReducer
+  //errors: errorsReducer
 });
 
 export default rootReducer;
