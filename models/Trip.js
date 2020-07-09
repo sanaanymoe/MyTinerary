a21917b6c3d19f1cda9fd700aae4d59de2f629a2
sanaanymoe <sanaany1992@gmail.com>
2020-07-09 15:36:58 -0700

    Backend wrap up

--- models/Trip.js
+++ models/Trip.js
@@ -1,102 +1,129 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-const FlightSchema = new Schema(
-    {
-    startLocation: {
-        type: String,
-        // required: true
-    },
-    endLocation: {
-        type: String,
-        // required: true
-    },
+// const FlightSchema = new Schema(
+//     {
+//     startLocation: {
+//         type: String,
+//         // required: true
+//     },
+//     endLocation: {
+//         type: String,
+//         // required: true
+//     },
 
-    startDate: {
-        type: Date,
-        default: Date.now()
-        // required: true
-    },
+//     startDate: {
+//         type: Date,
+//         default: Date.now()
+//         // required: true
+//     },
 
-    endDate: {
-        type: Date,
-        default: Date.now()
-        // required: true
-    },
+//     endDate: {
+//         type: Date,
+//         default: Date.now()
+//         // required: true
+//     },
 
-    price: {
-        type: String,
-        // required: true
-    },
+//     price: {
+//         type: String,
+//         // required: true
+//     },
 
-    bookingUrl: {
-        type:String,
-        // required: true
-    } 
+//     bookingUrl: {
+//         type:String,
+//         // required: true
+//     } 
 
-});
+// });
 
-const HousingSchema = new Schema({
-    location: {
-        type: String,
-        // required: true
-    },
+// const HousingSchema = new Schema({
+//     location: {
+//         type: String,
+//         // required: true
+//     },
 
-    price: {
-        type: String,
-        // required: true
-    },
+//     price: {
+//         type: String,
+//         // required: true
+//     },
 
-    startDate: {
-        type: Date,
-        default: Date.now()
-        // required: true
-    },
+//     startDate: {
+//         type: Date,
+//         default: Date.now()
+//         // required: true
+//     },
 
-    endDate: {
-        type: Date,
-        default: Date.now()
-        // required: true
-    }
+//     endDate: {
+//         type: Date,
+//         default: Date.now()
+//         // required: true
+//     }
 
-});
+// });
 
-const EventSchema = new Schema({
-    location: {
-        type: String,
-        // required: true
-    },
+// const EventSchema = new Schema({
+//     location: {
+//         type: String,
+//         // required: true
+//     },
 
-    price: {
-        type: String,
-        // required: true
-    },
+//     price: {
+//         type: String,
+//         // required: true
+//     },
 
-    startDate: {
-        type: Date,
-        default: Date.now()
-    },
+//     startDate: {
+//         type: Date,
+//         default: Date.now()
+//     },
 
-    endDate: {
-        type: Date,
-        // required: true
-        default: Date.now()
-    }
-})
+//     endDate: {
+//         type: Date,
+//         // required: true
+//         default: Date.now()
+//     }
+// })
 
-const TripSchema = new Schema({
+// const TripSchema = new Schema({
+//     user: {
+//         type: Schema.Types.ObjectId,
+//         ref: 'users'
+//     },
+   
+//     // flight object
+//     flight: FlightSchema,
+//     housing: HousingSchema,
+//     event: EventSchema
+
+
+// })
+
+var Trip = mongoose.model("trip", {
     user: {
         type: Schema.Types.ObjectId,
-        ref: 'users'
+        ref: "users",
     },
-    // flight object
-    flight: FlightSchema,
-    housing: HousingSchema,
-    event: EventSchema
+    
+  title: String,
+
+  flight: {
+    startLocation: String,
+    endLocation: String,
+    price: String,
+  },
+
+  housing: {
+    location: String,
+    price: String,
+  },
+
+  event: {
+    location: String,
+    price: String,
+  },
+});
 
 
-})
-
-const Trip = mongoose.model('trip', TripSchema);
+// const Trip = mongoose.model('trip', TripSchema);
 
 module.exports = Trip;
