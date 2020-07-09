03a53f4bcb9236d355178961b87675a857ad1fbf
sanaanymoe <sanaany1992@gmail.com>
2020-07-09 09:48:05 -0700

    add some styling

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -8,9 +8,20 @@ import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import NavBarContainer from './nav/navbar_container'
 import Trip from './trips/trip'
+import {Button} from 'react-bootstrap'
 
 const App = () => (
   <div>
+    {/* <>
+      <Button variant="primary">Primary</Button>{" "}
+      <Button variant="secondary">Secondary</Button>{" "}
+      <Button variant="success">Success</Button>{" "}
+      <Button variant="warning">Warning</Button>{" "}
+      <Button variant="danger">Danger</Button>{" "}
+      <Button variant="info">Info</Button>{" "}
+      <Button variant="light">Light</Button>{" "}
+      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
+    </> */}
     <NavBarContainer />
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
