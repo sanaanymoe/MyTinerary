8bf55277ada361cf419fe5d749970b73e812ad61
sanaanymoe <sanaany1992@gmail.com>
2020-07-08 17:58:52 -0700

    add trip utils

--- routes/api/trips.js
+++ routes/api/trips.js
@@ -3,10 +3,26 @@ const router = express.Router();
 const passport = require('passport');
 
 const Trip =require('../../models/Trip');
-const validateTripInput = require('../../validation/trips')
+const validateTripInput = require('../../validation/trips');
+const trips = require('../../validation/trips');
 
 router.get("/test", (req, res) => {
     res.json({msg: "This is trip route"});
+});
+
+router.get('/', (req, res) => {
+    Trip.find()
+        .then(trips => res.json(trips))
+        .catch(err => res.status(404).json({ notripsfound: 'No trips found' }));
+});
+
+router.get('/user/:user_id', (req, res) => {
+    Trip.find({user: req.params.user_id})
+        .then(trips => res.json(trips))
+        .catch(err =>
+            res.status(404).json({ notripsfound: 'No trips found for that user' }
+        )
+    );
 });
 
 router.post("/", 
